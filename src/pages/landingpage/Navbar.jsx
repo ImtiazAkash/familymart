2360b349c2afa667e74c1a609e251747a5c32c1d
ImtiazAkash <imtiaz.atick07@gmail.com>
2023-05-15 23:12:09 +0600

    second commit

--- src/pages/landingpage/Navbar.jsx
+++ src/pages/landingpage/Navbar.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import Class from "../../styles/Navbar.module.css"
-import logo from "../../assests/images/logo3.png"
+import React from "react";
+import Class from "../../styles/Navbar.module.css";
+import logo from "../../assests/images/logo3.png";
+import { AiOutlineShoppingCart, AiOutlineUser } from "react-icons/ai";
 
 function Navbar() {
   return (
     <div>
       <div className={Class.Navbar}>
         <div className={Class.logo}>
-          <img src={logo} alt="" />
+          <span>FamilyMart</span>
         </div>
 
-        
+        <form className={Class.searchContainer}>
+          <input className={Class.searchInput} type="text" placeholder="Search..." />
+          <button className={Class.searchButton} type="submit">Go</button>
+        </form>
+
         <div className={Class["right-side"]}>
           <button className={Class.userbtn} to="#login">
-            Login
+            <AiOutlineUser />
           </button>
-          <button className={Class.userbtn} to="#signup">
-            Sign Up
+          <button className={Class.cartbtn} to="#signup">
+            <AiOutlineShoppingCart />
           </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
